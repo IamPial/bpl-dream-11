c9704b763fe882ba8521221a6c207f549eed071c
Pial <pialname394@gmail.com>
2026-03-25 18:22:47 +0600

    added json this file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { Suspense } from "react";
 import Navbar from "./components/navbar/Navbar";
 import Banner from "./components/homepage/Banner";
+import Players from "./components/players/Players";
+
+const fetchPlayer = async () => {
+  const res = await fetch("/data.json");
+  return res.json();
+};
 
 const App = () => {
+  const playerPromise = fetchPlayer();
   return (
     <div>
       <Navbar />
       <Banner />
+      <Suspense
+        fallback={<span className="loading loading-bars loading-xl"></span>}
+      >
+        <Players playerPromise={playerPromise} />
+      </Suspense>
     </div>
   );
 };
