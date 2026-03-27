e15b1969fb28c8455e18b6f06d605ae8b55575e0
Pial <pialname394@gmail.com>
2026-03-27 16:13:43 +0600

    create simple selected section

---
+++ src/components/players/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SelectedPlayers = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <h2>title</h2>
+            <p>style</p>
+          </div>
+        </div>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default SelectedPlayers;
