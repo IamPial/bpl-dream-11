b8f4daa2bf96754422e57cb3b2788ee0ef27a271
Pial <pialname394@gmail.com>
2026-03-27 16:12:00 +0600

    pass the setCoin as a props

--- src/components/players/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/players/AvailablePlayers/AvailablePlayers.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Card from "../../ui/Cards/Card";
+import Card from "../../ui/Card/Card";
 
-const AvailablePlayers = ({ playerData }) => {
+const AvailablePlayers = ({ playerData, setCoin }) => {
   return (
     <div>
       <div className="grid grid-cols-3 gap-5">
         {playerData.map((player, index) => {
-          return <Card key={index} player={player} />;
+          return <Card key={index} player={player} setCoin={setCoin} />;
         })}
       </div>
     </div>
