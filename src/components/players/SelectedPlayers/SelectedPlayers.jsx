e51b1ac251862347516bb6d940771e80171e7b00
Pial <pialname394@gmail.com>
2026-03-27 18:25:06 +0600

    create selected Player markup with styles

--- src/components/players/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/players/SelectedPlayers/SelectedPlayers.jsx
@@ -1,18 +1,35 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
 
 const SelectedPlayers = ({ selectedPlayers }) => {
   console.log(selectedPlayers);
   return (
     <div>
-      <div>
-        <div>
-          <img src="" alt="" />
-          <div>
-            <h2>title</h2>
-            <p>style</p>
-          </div>
-        </div>
-        <button>Delete</button>
+      <div className=" space-y-5">
+        {selectedPlayers.map((player) => {
+          return (
+            <div className="border border-gray-200 p-4 rounded-xl">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-5">
+                  <img
+                    src={player.playerImg}
+                    className="h-20 w-25 rounded-xl"
+                    alt={player.playerName}
+                  />
+                  <div>
+                    <h2 className="font-bold text-lg">{player.playerName}</h2>
+                    <p className="font-semibold text-gray-500">
+                      {player.playerType}
+                    </p>
+                  </div>
+                </div>
+                <button className="btn text-error">
+                  <MdDelete />
+                </button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
