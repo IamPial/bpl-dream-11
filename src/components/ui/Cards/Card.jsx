983267f7960857b98b01540001f0c55ac2e6659f
Pial <pialname394@gmail.com>
2026-03-27 11:07:43 +0600

    created ui components for reusable components

---
+++ src/components/ui/Cards/Card.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaUser, FaFlag } from "react-icons/fa";
+
+const Card = ({ player }) => {
+  return (
+    <div>
+      <div className="p-4 border border-gray-200 rounded-xl">
+        <div className="card bg-base-100 ">
+          <figure>
+            <img
+              className="h-60 w-full rounded-xl"
+              src={player.playerImg}
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body px-0">
+            <h2 className="card-title">
+              <FaUser /> {player.playerName}
+            </h2>
+            <div className="flex items-center gap-2 justify-between">
+              <div className="flex items-center gap-2">
+                <FaFlag />
+                <p className="">{player.playerCountry}</p>
+              </div>
+              <button className="btn">{player.playerType}</button>
+            </div>
+            <div className="divider"></div>
+            <p className="font-semibold">Rating - {player.playerRating}</p>
+            <div className="flex justify-between">
+              <p className="font-semibold">{player.battingStyle}</p>
+              <p className="text-right">{player.bowlingStyle}</p>
+            </div>
+            <div className="card-actions justify-end">
+              <p className="font-semibold">Price : $ {player.playerPrice} </p>
+              <button className="btn ">Choose Player</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
