68ea6170d1bb11712e00e0725af7a917d8db1a03
Pial <pialname394@gmail.com>
2026-03-27 16:04:02 +0600

    passing setCoin  as a props to the child components

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
           </div>
         }
       >
-        <Players playerPromise={playerPromise} />
+        <Players playerPromise={playerPromise} setCoin={setCoin} />
       </Suspense>
     </div>
   );
