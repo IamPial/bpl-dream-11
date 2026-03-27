09bc6e5b27131354f53eab0890109c6fc3ec57ff
Pial <pialname394@gmail.com>
2026-03-27 17:37:58 +0600

    pass some props

--- src/components/players/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/players/AvailablePlayers/AvailablePlayers.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Card from "../../ui/Card/Card";
+import SelectedPlayers from "./../SelectedPlayers/SelectedPlayers";
 
-const AvailablePlayers = ({ playerData, setCoin, coin }) => {
+const AvailablePlayers = ({
+  playerData,
+  setCoin,
+  coin,
+  selectedPlayers,
+  setSelectedPlayers,
+}) => {
   return (
     <div>
       <div className="grid grid-cols-3 gap-5">
         {playerData.map((player, index) => {
           return (
-            <Card key={index} player={player} setCoin={setCoin} coin={coin} />
+            <Card
+              key={index}
+              player={player}
+              setCoin={setCoin}
+              coin={coin}
+              selectedPlayers={selectedPlayers}
+              setSelectedPlayers={setSelectedPlayers}
+            />
           );
         })}
       </div>
