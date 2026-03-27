f1c77d83e2cc34720c4fcf605db94b2301d5b5d1
Pial <pialname394@gmail.com>
2026-03-27 11:53:06 +0600

    created btn for toggling with update state

--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { use } from "react";
+import { use, useState } from "react";
 import AvailablePlayers from "./AvailablePlayers/AvailablePlayers";
 
 const Players = ({ playerPromise }) => {
   const playerData = use(playerPromise);
-  // console.log(data);
+  const [selectedType, setSelectedType] = useState("available");
   return (
-    <div className="container mx-auto pt-20">
+    <div className="container mx-auto py-14">
+      <div className="py-5 flex justify-between items-center">
+        <h2 className="text-3xl font-bold">Available Player</h2>
+        <div className="flex items-center">
+          <button
+            onClick={() => setSelectedType("available")}
+            className={`btn ${selectedType === "available" ? "bg-[#E7FE29]" : "bg-base-100"} rounded-r-none rounded-l-xl`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => setSelectedType("selected")}
+            className={`btn ${selectedType === "selected" ? "bg-[#E7FE29]" : "bg-base-100"} rounded-l-none rounded-r-xl`}
+          >
+            Selected(0)
+          </button>
+        </div>
+      </div>
+
       <AvailablePlayers playerData={playerData} />
     </div>
   );
