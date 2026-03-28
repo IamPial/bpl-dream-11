57a2b853254073180bcf6c2341575311c0ceeca1
Pial <pialname394@gmail.com>
2026-03-28 15:45:52 +0600

    added some styles to the footer section

--- src/components/homepage/Footer.jsx
+++ src/components/homepage/Footer.jsx
@@ -3,35 +3,45 @@ import footerLogo from "../../assets/logo-footer.png";
 
 const Footer = () => {
   return (
-    <div className="bg-[#06091A]">
-      <div className="container mx-auto pt-40">
+    <div className="bg-[#06091A] py-10">
+      <div className="container mx-auto pt-30">
         <div className="flex justify-center pb-12">
           <img src={footerLogo} alt="BPL LOGO" />
         </div>
-        {/* TODO: work with footer design */}
-        <div className="grid grid-cols-3 gap-3">
-          <div className="flex flex-col space-y-3">
+
+        <div className="grid grid-cols-3 gap-3 justify-between">
+          <div className="flex flex-col space-y-3 ">
             <h2 className="font-semibold text-lg text-white">About Us</h2>
-            <p className="leading-6 text-white ">
-              We are a passionate team dedicated to providing the best services
-              to our customers.
+            <p className="leading-6 text-white opacity-60 ">
+              We are a passionate team <br /> dedicated to providing the best{" "}
+              <br /> services to our customers.
             </p>
           </div>
           <div>
-            <h2>Quick Links</h2>
-            <ul>
+            <h2 className="font-semibold text-lg text-white mb-4">
+              Quick Links
+            </h2>
+            <ul className="list-disc text-white opacity-60 ml-8 space-y-2">
               <li>Home</li>
               <li>Services</li>
               <li>About</li>
               <li>Contact</li>
             </ul>
           </div>
-          <div>
-            <h2>Subscribe</h2>
-            <p>Subscribe to our newsletter for the latest updates.</p>
-            <div>
-              <input type="text" placeholder="Enter your email" />
-              <button className="btn ">Subscribe</button>
+          <div className="">
+            <h2 className="font-semibold text-lg text-white">Subscribe</h2>
+            <p className="leading-6 text-white opacity-60 my-5">
+              Subscribe to our newsletter for the <br /> latest updates.
+            </p>
+            <div className="flex items-center ">
+              <input
+                className="py-3 px-5 bg-white rounded-r-none rounded-xl border-none"
+                type="text"
+                placeholder="Enter your email"
+              />
+              <button className="btn shadow-none btn-lg rounded-l-none rounded-xl bg-linear-to-r from-yellow-500 to-pink-500">
+                Subscribe
+              </button>
             </div>
           </div>
         </div>
