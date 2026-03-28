e3e85da07af6a398eb8e83095a9cc537878855cb
Pial <pialname394@gmail.com>
2026-03-28 17:41:56 +0600

    added some classes for responsive styles

--- src/components/homepage/Subscribe.jsx
+++ src/components/homepage/Subscribe.jsx
@@ -3,19 +3,19 @@ import bgShadow from "../../assets/bg-shadow.png";
 
 const Subscribe = () => {
   return (
-    <div className="py-30 relative">
-      <div className="container mx-auto p-6 border border-white rounded-xl  absolute -bottom-40 left-10 z-10 backdrop-brightness-150 ">
+    <div className="py-30  relative">
+      <div className="container mx-auto p-6 border border-white rounded-xl  absolute -bottom-40 left-0 sm:left-4 md:left-8 lg:left-10 z-10 backdrop-brightness-150  ">
         <div
-          className="flex justify-center bg-white  items-center flex-col rounded-xl py-20   bg-cover bg-center bg-no-repeat"
+          className="flex justify-center   bg-white  items-center flex-col rounded-xl py-5 md:py-20 bg-cover bg-center bg-no-repeat"
           style={{ backgroundImage: `url(${bgShadow})` }}
         >
-          <h2 className="text-[32px] font-bold text-slate-900">
+          <h2 className="text-center md:text-left text-[32px] font-bold text-slate-900">
             Subscribe to our Newsletter
           </h2>
-          <p className="font-medium text-lg text-gray-500 mt-4">
+          <p className="text-center md:text-left font-medium text-lg text-gray-500 mt-4">
             Get the latest updates and news right in your inbox!
           </p>
-          <div className="flex w-145 items-center gap-3 mt-6">
+          <div className="flex flex-col md:flex-row w-full md:w-145 items-center gap-3 mt-6">
             <input
               className="flex-1 px-6 py-4 rounded-xl border border-gray-300"
               type="text"
