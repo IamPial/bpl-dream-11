7680152b8062d2133ded29992f4a6747ec7e2ffc
Pial <pialname394@gmail.com>
2026-03-28 17:42:36 +0600

    added some classes for responsiveness

--- src/components/players/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/players/AvailablePlayers/AvailablePlayers.jsx
@@ -11,7 +11,7 @@ const AvailablePlayers = ({
 }) => {
   return (
     <div>
-      <div className="grid grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5">
         {playerData.map((player, index) => {
           return (
             <Card
