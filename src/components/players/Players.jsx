765eaacb0d5419ad4986c27ec70f0c18bff96e43
Pial <pialname394@gmail.com>
2026-03-28 17:43:06 +0600

    added some classes for responsiveness

--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -9,24 +9,24 @@ const Players = ({ playerPromise, setCoin, coin }) => {
   const [selectedPlayers, setSelectedPlayers] = useState([]);
   return (
     <div className="container mx-auto py-14">
-      <div className="py-5 flex justify-between items-center">
+      <div className="py-5 flex justify-between items-center px-2 md:px-0">
         {selectedType === "available" ? (
-          <h2 className="text-3xl font-bold">Available Player</h2>
+          <h2 className="text-2xl md:text-3xl  font-bold">Available Player</h2>
         ) : (
-          <h2 className="text-3xl font-bold">
+          <h2 className="text-2xl md:text-3xl  font-bold">
             Selected Player ({selectedPlayers.length}/{playerData.length})
           </h2>
         )}
         <div className="flex items-center">
           <button
             onClick={() => setSelectedType("available")}
-            className={`btn ${selectedType === "available" ? "bg-[#E7FE29] font-bold" : "bg-base-100 font-normal"} rounded-r-none rounded-l-xl`}
+            className={`btn btn-sm md:btn-md ${selectedType === "available" ? "bg-[#E7FE29] font-bold" : "bg-base-100 font-normal"} rounded-r-none rounded-l-xl`}
           >
             Available
           </button>
           <button
             onClick={() => setSelectedType("selected")}
-            className={`btn ${selectedType === "selected" ? "bg-[#E7FE29] font-bold" : "bg-base-100 font-normal"} rounded-l-none rounded-r-xl`}
+            className={`btn btn-sm md:btn-md ${selectedType === "selected" ? "bg-[#E7FE29] font-bold" : "bg-base-100 font-normal"} rounded-l-none rounded-r-xl`}
           >
             Selected({selectedPlayers.length})
           </button>
